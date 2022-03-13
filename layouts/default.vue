57145efbe354fa329393eaec1d8e1e6a3c6b8898
KreskoLab <redeezol@gmail.com>
2022-03-13 17:54:10 +0200

    feat: add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-screen overflow-auto bg-slate-900">
+    <Navbar />
+
+    <div class="flex space-x-2 container mx-auto px-4 sm:px-0 lg:px-8 pt-10">
+      <Sidebar />
+      <slot/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+
+}
+</script>
+
+<style scoped>
+
+</style>
