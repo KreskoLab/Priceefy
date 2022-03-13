8e61f51e6d4a961fc1677c555d3953a63ab8861e
KreskoLab <redeezol@gmail.com>
2022-03-13 17:51:54 +0200

    feat(navbar): init component

---
+++ components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="sticky top-0 h-16 z-50 w-full border-b border-slate-800 bg-slate-900/75 backdrop-blur">
+      <div class="flex items-center container mx-auto px-4 sm:px-0 lg:px-8 h-full">
+        <p class="text-xl text-slate-100 font-bold">Pricify</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+
+</style>
