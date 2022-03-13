dfa9e72943b89541906473320fca352cd5533c10
KreskoLab <redeezol@gmail.com>
2022-03-13 17:52:40 +0200

    feat(sidebar): init component

---
+++ components/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sticky top-28 h-full w-1/4 hidden sm:block">
+    <div class="text-slate-100 text-xl">Магазини</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar"
+}
+</script>
+
+<style scoped>
+
+</style>
