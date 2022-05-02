02a47d6cc216710beb439845a85a530ff65a3aac
KreskoLab <redeezol@gmail.com>
2022-05-02 17:46:15 +0300

    ✨feat (app): add components

--- app.vue
+++ app.vue
@@ -1,7 +1,38 @@
+<script
+	setup
+	lang="ts"
+>
+const showBottomSheet = useBottomSheet()
+</script>
+
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage/>
-    </NuxtLayout>
-  </div>
+	<div
+		class="h-full bg-slate-900"
+		:class="showBottomSheet ? 'overflow-y-hidden' : 'overflow-y-auto'"
+	>
+		<NavBar />
+
+		<BottomSheet />
+
+		<div class="container mx-auto">
+			<div class="hidden xl:block">
+				<SideBar v-if="$route.name !== 'product'" />
+			</div>
+
+			<main class="pt-[4.5rem] lg:px-8 lg:pt-24">
+				<NuxtPage />
+			</main>
+		</div>
+	</div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+	transition: opacity 0.3s;
+}
+.page-enter,
+.page-leave-to {
+	opacity: 0;
+}
+</style>
