240cca3bbe604e11437c4b3da9929e4f88d22a87
KreskoLab <redeezol@gmail.com>
2022-05-02 17:43:17 +0300

    ✨feat: add Logo component

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,16 +1,14 @@
 <template>
-    <NuxtLink class="inline-flex items-center" to="/">
-      <img width="64" height="64" src="~/assets/logo.svg" alt="pricify logo"/>
-      <span class="text-xl text-slate-100 font-medium hidden md:block md:w-full">Pricify</span>
-    </NuxtLink>
+	<NuxtLink
+		class="inline-flex items-center"
+		to="/"
+	>
+		<img
+			width="64"
+			height="64"
+			src="@/assets/logo.svg"
+			alt="priceefy logo"
+		/>
+		<span class="text-xl text-slate-100 font-medium hidden md:block md:w-full">Priceefy</span>
+	</NuxtLink>
 </template>
-
-<script>
-export default {
-  name: "Logo"
-}
-</script>
-
-<style scoped>
-
-</style>
