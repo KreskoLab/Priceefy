53efa2bd550c01f153190f2e49dc09d35781922a
KreskoLab <redeezol@gmail.com>
2022-05-02 17:42:09 +0300

    ✨feat: add Badge component

---
+++ components/Badge.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col rounded-md w-full lg:w-40 bg-slate-700/90 pt-0">
+        <div class="w-max bg-slate-800/50 self-start rounded-br-lg rounded-tl-lg px-3 mb-1">
+            <span class="uppercase text-slate-400 text-xs tracking-wider leading-6">{{header}}</span>
+        </div>
+
+        <div class="flex justify-center items-center mb-2">
+            <span class="text-slate-300 text-center capitalize font-medium">{{text}}</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+    header: string,
+    text: string
+}>()
+</script>
