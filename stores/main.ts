480ffd6081963aaaefc58c17fd916b8d20cddd30
KreskoLab <redeezol@gmail.com>
2022-05-04 00:27:37 +0300

    feat: create pinia store

--- stores/main.ts
+++ stores/main.ts
@@ -1,32 +1,31 @@
+import { defineStore } from 'pinia'
 import type { Store } from '~/models/store'
 import type { Category } from '~/models/category'
 import type { City } from '~/models/city'
 
-const STATE = {
-	stores: <Store[]>[],
-	store: <Store>{},
-	category: <Category>{},
-	city: <City>{},
-}
-
-export const { state, mutation, getter, ...store } = createStore('main', STATE)
-
-export const setStore = mutation<Store>('setStore', (state, payload) => {
-	state.store = payload
-})
-
-export const setStores = mutation<Store[]>('setStores', (state, payload) => {
-	state.stores = payload
-})
-
-export const setCategory = mutation<Category>('setCategory', (state, payload) => {
-	state.category = payload
-})
-
-export const setCity = mutation<City>('setCity', (state, payload) => {
-	state.city = payload
-})
-
-export const availableStores = getter('availbaleStores', state => {
-	return state.stores.filter(store => store.cities.some(item => item.slug === state.city.slug))
+export const useStore = defineStore('main', {
+	state: () => ({
+		stores: [] as Store[],
+		store: {} as Store,
+		category: {} as Category,
+		city: {} as City,
+	}),
+	actions: {
+		setStore(newStore: Store) {
+			this.store = newStore
+		},
+		setStores(newStores: Store[]) {
+			this.stores = newStores
+		},
+		setCategory(newCategory: Category) {
+			this.category = newCategory
+		},
+		setCity(newCity: City) {
+			this.city = newCity
+		},
+	},
+	getters: {
+		availableStores: state =>
+			state.stores.filter(store => store.cities.some(item => item.slug === state.city.slug)),
+	},
 })
