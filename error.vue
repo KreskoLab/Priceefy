52b0f97b4bd0fbb95135e43c5330de2a527bf274
KreskoLab <redeezol@gmail.com>
2022-05-04 19:13:18 +0300

    ✨ feat: add page for errors handling

---
+++ error.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="relative font-sans h-screen bg-slate-900">
+		<div
+			class="flex flex-col justify-center items-center h-full container mx-auto py-10 px-4 text-slate-200"
+		>
+			<h1>Помилка сервера 😱</h1>
+			<h2>{{ error.statusCode }}</h2>
+		</div>
+	</div>
+</template>
+
+<script
+	setup
+	lang="ts"
+>
+defineProps({
+	error: Object,
+})
+</script>
+
+<style scoped>
+h1,
+h2 {
+	font-size: 3rem;
+}
+</style>
