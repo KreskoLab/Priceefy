94896b930d627fa1d8c364e6a530ded6c243daf6
KreskoLab <redeezol@gmail.com>
2022-05-04 18:09:32 +0300

    ✨ feat (weight): add handler for gram unit

--- composables/weight.ts
+++ composables/weight.ts
@@ -6,6 +6,9 @@ export const useHandleWeight = (weight: number, unit: string) => {
 		case 'ml':
 			return `${weight / 1000} л`
 
+		case 'g':
+			return `${weight} г`
+
 		case 'pcs':
 			if (weight > 5) return `${weight} г`
 			else return `${weight} шт`
