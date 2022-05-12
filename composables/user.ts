784fee122af96b7657b589c913b2ee02a01b0d83
KreskoLab <redeezol@gmail.com>
2022-05-13 00:09:15 +0300

    feat: add user composable

---
+++ composables/user.ts
@@ -0,0 +1,40 @@
+import { User } from '~/models/user'
+
+export const useUser = () => {
+	const user = useState('user', () => ({
+		loggedIn: false,
+		user: {} as User,
+	}))
+
+	return user
+}
+
+export const logIn = async () => {
+	const userStore = useUser()
+
+	const res = await $fetch('/api/user', {
+		headers: useRequestHeaders(['cookie']),
+		credentials: 'include',
+	})
+
+	if (res.status) {
+		userStore.value.user = res.user
+		userStore.value.loggedIn = true
+	} else {
+		userStore.value.user = {} as User
+		userStore.value.loggedIn = false
+	}
+}
+
+export const logOut = async () => {
+	const user = useUser()
+
+	await $fetch
+		.raw('http://localhost:8000/users/auth/logout', {
+			credentials: 'include',
+		})
+		.then(() => {
+			user.value.loggedIn = false
+			user.value.user = {} as User
+		})
+}
