1968cf3fdac406f65ffbe64385b42cfd8bf0358d
KreskoLab <redeezol@gmail.com>
2022-05-16 00:07:09 +0300

    feat: update pinia store

--- stores/main.ts
+++ stores/main.ts
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia'
 import type { Store } from '~/models/store'
 import type { Category } from '~/models/category'
 import type { City } from '~/models/city'
+import type { Sort } from '~/models/sort'
 
 export const useStore = defineStore('main', {
 	state: () => ({
@@ -9,19 +10,32 @@ export const useStore = defineStore('main', {
 		store: {} as Store,
 		category: {} as Category,
 		city: {} as City,
+		sort: { title: 'Дешевші', val: 'asc' } as Sort,
+		showDropdown: false,
 	}),
 	actions: {
 		setStore(newStore: Store) {
 			this.store = newStore
 		},
+
 		setStores(newStores: Store[]) {
 			this.stores = newStores
 		},
+
 		setCategory(newCategory: Category) {
 			this.category = newCategory
 		},
+
 		setCity(newCity: City) {
 			this.city = newCity
+		},
+
+		setSort(newSort: Sort) {
+			this.sort = newSort
+		},
+
+		openDropdown() {
+			this.showDropdown = !this.showDropdown
 		},
 	},
 	getters: {
