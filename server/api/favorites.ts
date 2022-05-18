1ffbc2bae5e253f74d90fdd59f2b5d4477b1eaf7
KreskoLab <redeezol@gmail.com>
2022-05-18 19:56:48 +0300

    fix (favorites): get api link from config

--- server/api/favorites.ts
+++ server/api/favorites.ts
@@ -3,15 +3,13 @@ import { Product } from '~/models/product'
 export default eventHandler(async event => {
 	const cookies = useCookies(event)
 	const query = useQuery(event)
+	const config = useRuntimeConfig()
 
-	const products = await $fetch<Product[]>(
-		`http://localhost:8000/users/${query.userId}/favorites`,
-		{
-			headers: {
-				cookie: `accessToken=${cookies.accessToken}`,
-			},
-		}
-	)
+	const products = await $fetch<Product[]>(`${config.baseAPI}/users/${query.userId}/favorites`, {
+		headers: {
+			cookie: `accessToken=${cookies.accessToken}`,
+		},
+	})
 
 	return products
 })
