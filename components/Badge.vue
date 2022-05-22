e59f25a55d9afeafd785083ea426fcbeb6bfea47
KreskoLab <redeezol@gmail.com>
2022-05-22 16:10:24 +0300

    feat (Badge): add light theme

--- components/Badge.vue
+++ components/Badge.vue
@@ -1,7 +1,7 @@
 <template>
-	<div class="flex flex-col rounded-md w-full lg:w-40 dark:bg-slate-700/90 bg-white pt-0">
+	<div class="flex flex-col rounded-md w-full lg:w-40 dark:bg-slate-700/90 bg-gray-100 pt-0">
 		<div
-			class="w-max dark:bg-slate-800/50 bg-gray-100/60 self-start rounded-br-lg rounded-tl-lg px-3 mb-1"
+			class="w-max dark:bg-slate-800/50 bg-gray-200 self-start rounded-br-lg rounded-tl-lg px-3 mb-1"
 		>
 			<span class="uppercase dark:text-slate-400 text-gray-500 text-xs tracking-wider leading-6">
 				{{ header }}
