716e04af3e1bf36875f4e423f130f155ed17cb08
KreskoLab <redeezol@gmail.com>
2022-05-25 11:38:21 +0300

    style (SkeletonLevel): add width for mobile

--- components/Skeleton/SkeletonLevel.vue
+++ components/Skeleton/SkeletonLevel.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex items-center justify-between animate-pulse pt-4 mb-8">
 		<div class="w-40 h-5 dark:bg-slate-700/90 bg-gray-200 rounded" />
-		<div class="w-40 h-5 dark:bg-slate-700/90 bg-gray-200 rounded" />
+		<div class="w-16 lg:w-40 h-5 dark:bg-slate-700/90 bg-gray-200 rounded" />
 	</div>
 </template>
