caec8e6b384771a069b880e53222198ca21fbb4a
KreskoLab <redeezol@gmail.com>
2022-05-28 19:52:51 +0300

    feat (BottomShett): remove swipes

--- components/BottomSheet.vue
+++ components/BottomSheet.vue
@@ -15,12 +15,8 @@
 				v-if="show"
 				ref="sheetBody"
 				class="flex flex-col space-y-6 rounded-t-3xl dark:bg-slate-900 bg-gray-300 mt-32 pt-8 px-6 h-[calc(100%-8rem)]"
-				:style="height > 0 ? `margin-top: ${height}px` : ''"
 			>
-				<div
-					ref="swipe"
-					class="h-[4px] w-24 mx-auto rounded-md bg-slate-200"
-				></div>
+				<div class="h-[4px] w-24 mx-auto rounded-md bg-slate-200" />
 
 				<SideBar />
 			</div>
@@ -34,39 +30,15 @@
 >
 const showBottomSheet = useBottomSheet()
 const show = ref<boolean>(true)
-const height = ref<number>(0)
 
 const sheetBody = ref<HTMLElement | null>(null)
-const swipe = ref<HTMLElement | null>(null)
-const sheetBodyWidth = computed(() => sheetBody.value?.offsetWidth)
 
 onMounted(() => {
 	onClickOutside(sheetBody, () => (show.value = false))
-
-	const { lengthY } = useSwipe(swipe, {
-		passive: false,
-		onSwipe(e: TouchEvent) {
-			if (sheetBodyWidth.value) {
-				if (lengthY.value < 0) {
-					height.value = Math.abs(lengthY.value) + 50
-				}
-			}
-		},
-		onSwipeEnd() {
-			if (
-				lengthY.value < 0 &&
-				sheetBodyWidth.value &&
-				Math.abs(lengthY.value) / sheetBodyWidth.value >= 0.4
-			) {
-				show.value = false
-			}
-		},
-	})
 })
 
 function hideBottomSheet() {
 	showBottomSheet.value = false
 	show.value = true
-	height.value = 0
 }
 </script>
