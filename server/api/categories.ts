5a862b35f437aefded1b65936d5a64dca89ca90a
KreskoLab <redeezol@gmail.com>
2022-07-11 00:32:15 +0300

    refactor (api): clean up & update bakend route

--- server/api/categories.ts
+++ server/api/categories.ts
@@ -2,14 +2,5 @@ import type { Category } from '~/models/category'
 
 export default defineEventHandler(async () => {
 	const config = useRuntimeConfig()
-
-	const res = await $fetch<object[]>(`${config.baseAPI}/category`)
-
-	const categories: Category[] = res.map((item: any) => ({
-		name: item.name,
-		slug: item.slug,
-		icon: item.icon,
-	}))
-
-	return categories
+	return $fetch<Category[]>(`${config.baseAPI}/categories`)
 })
