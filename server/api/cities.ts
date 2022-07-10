37dad752fdb54c218872c6f38eb0cfe4883028ba
KreskoLab <redeezol@gmail.com>
2022-07-11 00:30:23 +0300

    refactor (api): update backend api route for cities

--- server/api/cities.ts
+++ server/api/cities.ts
@@ -1,10 +1,10 @@
 import type { City } from '~/models/city'
 import type { SelectOption } from '~~/models/select-option'
 
-export default defineEventHandler(async event => {
+export default defineEventHandler(async () => {
 	const config = useRuntimeConfig()
 
-	const cities = await $fetch<City[]>(`${config.baseAPI}/store/cities`)
+	const cities = await $fetch<City[]>(`${config.baseAPI}/cities`)
 	const options: SelectOption[] = cities.map(city => ({ title: city.name, value: city }))
 
 	return options
