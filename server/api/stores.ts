eff3399cf0b2886405878250764c05e7ef5914c5
KreskoLab <redeezol@gmail.com>
2022-07-11 00:30:58 +0300

    refactor (api): clean up and update backend route

--- server/api/stores.ts
+++ server/api/stores.ts
@@ -2,15 +2,5 @@ import type { Store } from '~/models/store'
 
 export default defineEventHandler(async () => {
 	const config = useRuntimeConfig()
-
-	const res = await $fetch<object[]>(`${config.baseAPI}/store`)
-
-	const stores: Store[] = res.map((item: any) => ({
-		name: item.name,
-		slug: item.slug,
-		icon: item.image,
-		cities: item.cities,
-	}))
-
-	return stores
+	return $fetch<Store[]>(`${config.baseAPI}/stores`)
 })
