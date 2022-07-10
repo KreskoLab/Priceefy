2ed06042b3532b4fcb3828ccb14f730da4ecee96
KreskoLab <redeezol@gmail.com>
2022-07-11 00:28:01 +0300

    feat (api): add interface for queryObject

--- server/api/products/[city].ts
+++ server/api/products/[city].ts
@@ -1,17 +1,35 @@
-import type { Product } from '~/models/product'
-import type { ProductsAndCount } from '~/models/products-count'
+import { ProductResponse } from '~/interfaces'
+
+interface Query {
+	page: number
+	sort: string
+	category?: string
+	store?: string
+}
 
 export default defineEventHandler(async event => {
 	const config = useRuntimeConfig()
-	const queryObject = useQuery(event)
+	const queryObject = useQuery(event) as unknown as Query
 
-	const res = await $fetch<ProductsAndCount>(`products/city/${event.context.params.city}`, {
+	let url = ''
+
+	if (queryObject.category || (queryObject.category && queryObject.store)) {
+		url = `/categories/${queryObject.category}?city=${event.context.params.city}`
+	} else if (queryObject.store) {
+		url = `/stores/${queryObject.store}?city=${event.context.params.city}`
+	} else {
+		url = `/products?city=${event.context.params.city}`
+	}
+
+	const res = await $fetch<ProductResponse>(url, {
 		params: queryObject,
 		baseURL: config.baseAPI,
+	}).then(res => {
+		return {
+			count: res.count,
+			results: res.results.map(({ category, country, ...rest }) => rest),
+		}
 	})
 
-	const products: Product[] = res.products.map(({ category, country, ...rest }) => rest)
-	const count: number = res.count
-
-	return { products, count }
+	return res
 })
