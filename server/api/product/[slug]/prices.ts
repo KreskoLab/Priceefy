e953f4ebada32fd449dc6c2a8e6f168e5f69f5ea
KreskoLab <redeezol@gmail.com>
2022-07-16 00:35:08 +0300

    refactor (prices): simplify code

--- server/api/product/[slug]/prices.ts
+++ server/api/product/[slug]/prices.ts
@@ -1,31 +1,11 @@
 import type { ChartSeries } from '~/models/chart-series'
+import { Price } from '~/models/price'
 
 export default defineEventHandler(async event => {
 	const config = useRuntimeConfig()
 	const query = useQuery(event)
 
-	try {
-		const chartSeries: ChartSeries = {
-			series: [],
-			timeseries: [],
-		}
-
-		const time = []
-
-		const res = await $fetch<[]>(
-			`${config.baseAPI}/products/${event.context.params.slug}/prices?city=${query.city}&period=${query.period}`
-		)
-
-		res.forEach((item: any) => {
-			const prices = item.prices.map(item => [item.createdAt, item.price])
-			const itemCreatedAt = item.prices.map(item => item.createdAt)
-
-			chartSeries.series.push({ name: item.store, data: prices })
-			time.push(itemCreatedAt)
-		})
-
-		chartSeries.timeseries = [...new Set(time.flat())]
-
-		return chartSeries
-	} catch (error) {}
+	return $fetch<Pick<Price, 'price' | 'store' | 'created_at'>[]>(
+		`${config.baseAPI}/products/${event.context.params.slug}/prices?city=${query.city}&period=${query.period}`
+	)
 })
