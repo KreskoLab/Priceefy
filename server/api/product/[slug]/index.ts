8cc7b737ad0d6c64cb5d74dbead1978010a79e9a
KreskoLab <redeezol@gmail.com>
2022-07-16 00:36:08 +0300

    refactor: code clean up

--- server/api/product/[slug]/index.ts
+++ server/api/product/[slug]/index.ts
@@ -5,11 +5,9 @@ export default defineEventHandler(async event => {
 	const query = useQuery(event)
 
 	try {
-		const res = await $fetch<Product>(
+		return $fetch<Product>(
 			`${config.baseAPI}/products/${event.context.params.slug}?city=${query.city}`
 		)
-
-		return res
 	} catch (error) {
 		throwError('Товар не знайден')
 	}
