3ba665c1e1329605afa42e1868ac2fb2829b7bc8
KreskoLab <redeezol@gmail.com>
2022-07-16 00:31:08 +0300

    refactor (AppChip): replace svg icon with icon component

--- components/App/AppChip.vue
+++ components/App/AppChip.vue
@@ -1,39 +1,26 @@
-<template>
-	<div
-		class="flex space-x-3 rounded-3xl dark:bg-slate-800 bg-white border dark:border-slate-700 border-gray-200 py-1 px-4 text-sm dark:text-slate-300 text-gray-600 w-max"
-	>
-		<span class="select-none truncate w-min">{{ title }}</span>
-
-		<button
-			class="outline-none"
-			@click="emit('clear')"
-		>
-			<svg
-				class="h-4 w-4 mt-[2px] text-slate-400"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				stroke-width="2"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M6 18L18 6M6 6l12 12"
-				/>
-			</svg>
-		</button>
-	</div>
-</template>
-
-<script
-	setup
-	lang="ts"
->
+<script setup lang="ts">
 defineProps<{
 	title: string
 }>()
 
 const emit = defineEmits<{
-	(e: 'clear'): void
+	(e: 'remove'): void
 }>()
 </script>
+
+<template>
+	<div
+		class="flex space-x-3 rounded-3xl dark:bg-slate-800 bg-white border dark:border-slate-700 border-gray-200 py-1.5 px-4 text-sm dark:text-slate-300 text-gray-600 w-max"
+	>
+		<p class="select-none truncate w-min">{{ title }}</p>
+
+		<button
+			class="outline-none"
+			@click="emit('remove')"
+		>
+			<IconMdiClose
+				class="text-slate-400 mt-[1px] hover:text-slate-600 dark:hover:text-slate-200"
+			/>
+		</button>
+	</div>
+</template>
