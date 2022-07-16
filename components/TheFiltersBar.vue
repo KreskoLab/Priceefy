894a28b17e5ae8883bb3f83ebc6a0b8fbb775c3f
KreskoLab <redeezol@gmail.com>
2022-07-16 17:34:55 +0300

    refactor (TheFiltersBar): replace svg with icon component

--- components/TheFiltersBar.vue
+++ components/TheFiltersBar.vue
@@ -1,19 +1,9 @@
 <template>
 	<div class="flex items-center space-x-4 w-full mt-3 h-10">
-		<svg
-			class="h-6 w-6 dark:text-slate-400 text-gray-600"
-			fill="none"
-			viewBox="0 0 24 24"
-			stroke="currentColor"
-			stroke-width="2"
+		<IconMdiFilterOutline
 			v-if="Object.keys(store).length || Object.keys(category).length"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
-			/>
-		</svg>
+			class="h-6 w-6 dark:text-slate-400 text-gray-600"
+		/>
 
 		<AppChip
 			:title="store.name"
@@ -29,10 +19,7 @@
 	</div>
 </template>
 
-<script
-	setup
-	lang="ts"
->
+<script setup lang="ts">
 import type { Category } from '~/models/category'
 import type { Store } from '~/models/store'
 import { useStore } from '~/stores/main'
