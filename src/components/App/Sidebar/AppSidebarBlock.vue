6403e319ad44b152928760e54817a04c2dd987db
KreskoLab <redeezol@gmail.com>
2022-07-18 23:55:08 +0300

    style: script setup first

--- src/components/App/Sidebar/AppSidebarBlock.vue
+++ src/components/App/Sidebar/AppSidebarBlock.vue
@@ -1,33 +1,8 @@
-<template>
-	<ul class="flex flex-col space-y-4 mt-3 mb-8">
-		<h2 class="dark:text-slate-100 text-gray-800 text-xl">{{ title }}</h2>
+<script setup lang="ts">
+import type { Store } from '~/models/store'
+import type { Category } from '~/models/category'
 
-		<li
-			v-for="item in items"
-			:key="item.slug"
-		>
-			<button
-				class="flex items-center w-full space-x-2 dark:bg-slate-800 bg-white border-t dark:border-t-slate-700 border-t-gray-200 outline-none dark:hover:bg-slate-400/20 hover:bg-white/60 rounded-md py-2.5 px-3 dark:text-slate-400 text-gray-600 transition duration-150 ease-in"
-				:class="item.slug === selected.slug ? 'btn_selected' : ''"
-				@click="select(item)"
-			>
-				<img
-					class="w-6 h-6 filter invert-[.60] dark:filter-none"
-					:src="`${config.baseAPI}/${item.icon}`"
-					:alt="item.name"
-				/>
-				<span class="float-left truncate">{{ item.name }}</span>
-			</button>
-		</li>
-	</ul>
-</template>
-
-<script
-	setup
-	lang="ts"
->
-import type { Store } from '@/models/store'
-import type { Category } from '@/models/category'
+const config = useRuntimeConfig()
 
 const props = defineProps<{
 	title: string
@@ -35,17 +10,15 @@ const props = defineProps<{
 	items: (Store | Category)[]
 }>()
 
-const config = useRuntimeConfig()
+const emit = defineEmits<{
+	(e: 'update:modelValue', selected: object): void
+}>()
 
 const selected = reactive<Store | Category>({
 	name: '',
 	icon: '',
 	slug: '',
 })
-
-const emit = defineEmits<{
-	(e: 'update:modelValue', selected: object): void
-}>()
 
 function select(item: Store | Category) {
 	if (selected.slug === item.slug) {
@@ -74,7 +47,38 @@ watch(
 )
 </script>
 
+<template>
+	<ul class="flex flex-col space-y-4 mt-3 mb-8">
+		<h2 class="dark:text-slate-100 text-gray-800 text-xl">{{ title }}</h2>
+
+		<li
+			v-for="item in items"
+			:key="item.slug"
+		>
+			<button
+				class="flex items-center w-full space-x-2 outline-none rounded-md py-2.5 px-3 dark:text-slate-400 text-gray-600 transition duration-150 ease-in"
+				:class="item.slug === selected.slug ? 'btn_selected' : ''"
+				@click="select(item)"
+			>
+				<img
+					class="w-6 h-6 filter invert-[.60] dark:filter-none"
+					:src="`${config.baseAPI}/${item.icon}`"
+					:alt="item.name"
+				/>
+				<span class="float-left truncate">{{ item.name }}</span>
+			</button>
+		</li>
+	</ul>
+</template>
+
 <style scoped>
+button {
+	@apply dark:bg-slate-800 bg-white
+	dark:hover:bg-slate-400/20 hover:bg-white/60
+	border-t dark:border-t-slate-700 border-t-gray-200
+	focus:ring-2 focus:ring-teal-400;
+}
+
 .btn_selected {
 	@apply ring-2 ring-teal-400
   dark:text-slate-300 text-gray-800;
