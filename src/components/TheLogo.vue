c0b739a1a6dca9830815e75fb6ea0446a9972e2d
KreskoLab <redeezol@gmail.com>
2022-07-24 00:33:43 +0300

    feat (TheLogo): add reset function

--- src/components/TheLogo.vue
+++ src/components/TheLogo.vue
@@ -1,5 +1,19 @@
+<script setup lang="ts">
+import { useStore } from '~/stores/main'
+import type { Category } from '~/models/category'
+import type { Store } from '~/models/store'
+
+const store = useStore()
+
+function reset() {
+	store.setCategory({} as Category)
+	store.setStore({} as Store)
+}
+</script>
+
 <template>
 	<NuxtLink
+		@click="reset()"
 		class="flex items-center"
 		to="/"
 	>
@@ -9,7 +23,7 @@
 			src="~/assets/logo.svg"
 			alt="priceefy logo"
 		/>
-		<span class="text-xl dark:text-slate-100 text-gray-800 font-medium hidden md:block md:w-full">
+		<span class="text-xl dark:text-slate-100 text-gray-800 font-medium hidden lg:block lg:w-full">
 			Priceefy
 		</span>
 	</NuxtLink>
