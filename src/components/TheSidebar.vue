dc39813bb7cc10cc760c78db89b1b5d3310d83f1
KreskoLab <redeezol@gmail.com>
2022-07-24 00:31:53 +0300

    fix (TheSidebar): useAsyncData instead of useFetch

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -3,6 +3,11 @@ import type { Category } from '~/models/category'
 import type { Store } from '~/models/store'
 import { useStore } from '~/stores/main'
 
+const [{ data: stores }, { data: categories }] = await Promise.all([
+	useAsyncData<Store[]>('stores', () => $fetch('/api/stores')),
+	useAsyncData<Category[]>('categories', () => $fetch('/api/categories')),
+])
+
 const piniaStore = useStore()
 const queryObject = useQueryObject()
 
@@ -16,21 +21,14 @@ const store = computed({
 	set: (value: Store) => piniaStore.setStore(value),
 })
 
-const [{ data: stores }, { data: categories }] = await Promise.all([
-	useFetch<Store[]>('/api/stores'),
-	useFetch<Category[]>('/api/categories'),
-])
-
 piniaStore.setStores(stores.value)
 
 store.value = findItemBySlug(piniaStore.availableStores, queryObject.store) || store.value
 category.value = findItemBySlug(categories.value, queryObject.category) || category.value
 
 function findItemBySlug(arr: (Store | Category)[], slug: string): Store | Category {
-	const item = arr.find(item => item.slug === slug)
-
+	const item = arr?.find(item => item.slug === slug)
 	if (item) return JSON.parse(JSON.stringify(item))
-	else return undefined
 }
 </script>
 
@@ -38,7 +36,10 @@ function findItemBySlug(arr: (Store | Category)[], slug: string): Store | Catego
 	<aside
 		class="h-full lg:overflow-y-hidden lg:hover:overflow-y-scroll lg:fixed lg:h-[calc(100%-5rem)] lg:top-20 lg:ml-6 lg:pb-10 scrollbar-thin dark:scrollbar-thumb-slate-700 scrollbar-thumb-gray-300 scrollbar-track-transparent scrollbar-thumb-rounded-md"
 	>
-		<div class="w-full px-3 lg:w-72 lg:px-6">
+		<div
+			class="w-full px-3 lg:w-72 lg:px-6"
+			v-if="categories && stores"
+		>
 			<SidebarBlock
 				v-model="store"
 				title="Магазини"
